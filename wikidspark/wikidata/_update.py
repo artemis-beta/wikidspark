b44cfa89c6786fa9539cae11e4fccf73b3d2d553
Kristian Zarebski <krizar312@gmail.com>
2020-08-09 18:47:40 +0100

    Added updating script

---
+++ wikidspark/wikidata/_update.py
@@ -0,0 +1,30 @@
+from wikidspark.query import get_by_id
+import wikidspark.exceptions
+import time # Must not overload server
+from tqdm import tqdm
+import json
+from os.path import exists, join, realpath, basename
+
+loc_dir = realpath(__file__).replace(basename(__file__), '')
+
+def _update_properties(x_l, x_u):
+    assert x_l > 16, "Lowest property is P17"
+    if exists(join(loc_dir, 'properties.json')):
+        _properties = json.load(open(join(loc_dir, 'properties.json')))
+    else:
+        _properties = {}
+    for i in tqdm(range(x_l, x_u), desc='Fetching Properties'):
+        if 'P{i}' in _properties:
+            continue
+        try:
+            result = get_by_id(f'P{i}')
+        except wikidspark.exceptions.IDNotFoundError:
+            continue
+        _properties[f'P{i}'] = result['labels']['en']['value']
+        time.sleep(2)
+    json.dump(_properties, open(join(loc_dir, 'properties.json'), 'w'))
+
+
+if __name__ in "__main__":
+    _update_properties(2501, 2600)
+
