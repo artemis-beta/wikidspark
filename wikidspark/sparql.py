2f8106846777dbb1f38dd093009ec28f65af0d0b
Kristian Zarebski <krizar312@gmail.com>
2022-08-06 19:45:29 +0100

    Remake of SPARQL module

--- wikidspark/sparql.py
+++ wikidspark/sparql.py
@@ -1,143 +1,97 @@
-from wikidspark.wikidata.meta import prefixes
+import typing
+
+
+import wikidspark.wikidata.meta as wkd_meta
 
 
 class SPARQL:
-    def __init__(self, item_label="item", language_id="en"):
-        self._clear()
-        self._selection = [item_label]
-        self._n_entries = -1
-        self._language = language_id
-        self._item = item_label
-        self._ordering = None
+    def __init__(self, language: str = "en", limit: int=100) -> None:
+        self._distinct: bool = False
+        self.language: str = language
+        self.limit: int = limit
+        self._where: typing.Dict[str, str] = {}
+        self._select_labels: typing.Dict[str, bool] = {}
+        self._group_by: typing.List[str] = []
+        self._order_by: typing.List[str] = []
 
-    def _select_str(self):
-        if not self._selection or not isinstance(self._selection, list):
-            raise AssertionError("Failed to retrieve SELECT string")
-        return "SELECT DISTINCT " + " ".join(f"?{s}" for s in self._selection)
+    @property
+    def DISTINCT(self) -> "SPARQL":
+        self._distinct = True
+        return self
 
-    def _where_str(self):
-        return (
-            (
-                "\t"
-                + "\n\t".join(
-                    [
-                        f'?{self._item} {prefixes["property"]}:{k} '
-                        + f'{prefixes["entity"]}:{v} .'
-                        for k, v in self._where.items()
-                    ]
-                )
-                + "\n"
-            )
-            if self._where
-            else ""
-        )
+    def _select_variable(self, property: str) -> str:
+        return f"?var{property}"
 
-    def _statement_str(self):
-        return (
-            (
-                "\t"
-                + "\n\t".join(
-                    [
-                        f'?item p:{prop} ?statement0 .\n'
-                        + f'?statement0 (ps:{prop}) "{value}" .'
-                        for prop, value in self._statements.items()
-                    ]
-                )
-                + "\n"
-            )
-            if self._statements
-            else ""
-        )
+    def WHERE(self, property: str, value: typing.Optional[str]=None, show: bool=True, label: bool=True) -> "SPARQL":
+        value = value or self._select_variable(property)
+        self._where[property] = value
+        if show:
+            self._select_labels[property] = label
+        return self
 
-    def _service_str(self):
-        if not self._service:
+    def GROUP_BY(self, property: str) -> "SPARQL":
+        self._group_by.append(property)
+        return self
+
+    def ORDER_BY(self, property: str) -> "SPARQL":
+        self._order_by.append(property)
+        return self
+
+    @property
+    def where_str(self) -> str:
+        if not self._where:
             return ""
-        _out_srv = [
-            f'\tSERVICE {s["tag"]}' + " {" + " ".join(s["args"].values()) + "} ."
-            for s in self._service
+        _out_str: str = "WHERE {"
+        _out_str += f"\n\t{self.service_str}\n\t"
+        _statements: typing.List[str] = [
+            f"{self._select_variable(k)} "
+            f"{wkd_meta.prefixes['property']}:{k} "
+            f"{wkd_meta.prefixes['entity']}:{v}."
+            for k, v in self._where.items()
         ]
-        return "\n\t".join(_out_srv) + "\n"
+        _out_str += "\n\t".join(_statements)
+        _out_str += "\n}"
+        
+        return _out_str
 
-    def _limit_str(self):
-        return "" if self._n_entries == -1 else f"\nLIMIT {self._n_entries}"
+    @property
+    def order_by_str(self) -> str:
+        return "ORDER BY " + " ".join(f"ASC(UCASE({self._select_variable(k)}Label))" for k in self._order_by)
 
-    def _filter_str(self):
-        if not self._filter:
-            return ""
-        _out_filt = [f"\tFILTER {f} ." for f in self._filter]
-        return "\n\t".join(_out_filt) + "\n"
+    @property
+    def service_str(self) -> str:
+        return 'SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],'+self.language+'". }'
 
-    def _order_str(self):
-        if not self._ordering:
-            return ""
-        _order_str = "ORDER BY "
-        _order_str += (
-            f"DESC(?{self._ordering[0]})"
-            if self._ordering[1]
-            else f"?{self._ordering[0]}"
-        )
-        _order_str += "\n"
-        return _order_str
+    @property
+    def group_by_str(self) -> str:
+        return "\n".join(f"GROUP BY {self._select_variable(i)}Label" for i in self._group_by)
 
-    def _clear(self):
-        self._where = {}
-        self._service = []
-        self._filter = []
-        self._statements = {}
+    @property
+    def limit_str(self) -> str:
+        return f"LIMIT {self.limit}"
 
-    def SELECT(self, *args):
-        if any("Label" in arg for arg in args):
-            self.SERVICE(
-                f'{prefixes["ontology"]}:label',
-                service_param=f'{prefixes["big-data"]}:serviceParam',
-                lang=f'{prefixes["ontology"]}:language "[AUTO_LANGUAGE], {self._language}"',
-            )
-        self._selection += [*args]
+    @property
+    def selection_str(self) -> str:
+        _select_str: str = "SELECT "
+        if self._distinct:
+            _select_str += "DISTINCT "
+        _select_str += "\n"
+        
+        for selection, show_label in self._select_labels.items():
+            if selection not in self._group_by:
+                _select_str += "\t"+self._select_variable(selection)
+            else:
+                _select_str += "\t"
+            if show_label:
+                _select_str += " "+self._select_variable(selection)+"Label"
+            _select_str += "\n"
 
-    def ORDER_BY(self, value, desc=False):
-        self._ordering = (value, desc)
-
-    def WHERE(self, **kwargs):
-        self._where.update(**kwargs)
-
-    def SERVICE(self, service_tag, **kwargs):
-        if {"tag": service_tag, "args": kwargs} not in self._service:
-            self._service += [{"tag": service_tag, "args": kwargs}]
-
-    def LIMIT(self, n_entries):
-        self._n_entries = n_entries
-
-    def FILTER_EXISTS(self, **kwargs):
-        for _, v in kwargs.items():
-            self._filter.append(
-                "EXISTS {?item" + prefixes["property"] + ":" + v + " } ."
-            )
-
-    def STATEMENT(self, **kwargs):
-        self._statements.update(**kwargs)
-
-    def FILTER_NOT_EXISTS(self, **kwargs):
-        for _, v in kwargs.items():
-            self._filter.append(
-                "NOT EXISTS {?item" + prefixes["property"] + ":" + v + " } ."
-            )
-
-    def FILTER_RELATION(self, relation="=", **kwargs):
-        for k, v in kwargs.items():
-            self._filter.append(f'( {prefixes["property"]}:{k} {relation} "{v}" ) .')
+        return _select_str
 
     def build(self) -> str:
-        _query_str = self._select_str()
-        _query_str += """
-WHERE
-{
-"""
-        _query_str += self._where_str()
-        _query_str += self._statement_str()
-        _query_str += self._service_str()
-        _query_str += self._filter_str()
-        _query_str += """
-}"""
-        _query_str += self._order_str()
-        _query_str += self._limit_str()
+        _query_str = self.selection_str
+        _query_str += f"\n{self.where_str}"
+        _query_str += f"\n{self.group_by_str}" if self.group_by_str else ""
+        _query_str += f"\n{self.order_by_str}" if self.order_by_str else ""
+        _query_str += f"\n{self.limit_str}"
         return _query_str
